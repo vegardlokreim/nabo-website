b312f3a0f5425898710f28cc0a825b287b415465
Vegard Lokreim <vegardlokreim9@gmail.com>
2024-08-21 13:45:59 +0200

    validering

--- src/components/MobileFooter.tsx
+++ src/components/MobileFooter.tsx
@@ -12,16 +12,7 @@ export default function Footer() {
                     </div>
                     <p className="text-[#181411] text-xs font-medium leading-normal tracking-[0.015em]">Hjem</p>
                 </a>
-                <a className="just flex flex-1 flex-col items-center justify-end gap-2 text-[#897261]" href="/meny">
-                    <div className="text-[#897261] flex h-8 items-center justify-center" data-icon="ShoppingBag" data-size="24px" data-weight="regular">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24px" height="24px" fill="currentColor" viewBox="0 0 256 256">
-                            <path
-                                d="M216,40H40A16,16,0,0,0,24,56V200a16,16,0,0,0,16,16H216a16,16,0,0,0,16-16V56A16,16,0,0,0,216,40Zm0,160H40V56H216V200ZM176,88a48,48,0,0,1-96,0,8,8,0,0,1,16,0,32,32,0,0,0,64,0,8,8,0,0,1,16,0Z"
-                            ></path>
-                        </svg>
-                    </div>
-                    <p className="text-[#897261] text-xs font-medium leading-normal tracking-[0.015em]">Meny</p>
-                </a>
+
                 <a className="just flex flex-1 flex-col items-center justify-end gap-2 text-[#897261]" href="/meny">
                     <div className="text-[#897261] flex h-8 items-center justify-center" data-icon="ShoppingBag" data-size="24px" data-weight="regular">
                         <svg xmlns="http://www.w3.org/2000/svg" width="24px" height="24px" fill="currentColor" viewBox="0 0 256 256">
