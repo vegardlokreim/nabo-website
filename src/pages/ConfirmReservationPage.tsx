9f15b001f2fa69db49c3290694a067cf754cc2c3
Vegard Lokreim <vegardlokreim9@gmail.com>
2024-08-29 21:53:31 +0200

    added note from customer on table reservation

--- src/pages/ConfirmReservationPage.tsx
+++ src/pages/ConfirmReservationPage.tsx
@@ -22,7 +22,8 @@ export type FirestoreReservation = {
     phone: string,
     guests: string,
     date: string,
-    isConfirmed: boolean
+    isConfirmed: boolean,
+    text?: string
 }
 
 function ConfirmReservationpage() {
@@ -83,6 +84,9 @@ function ConfirmReservationpage() {
                 <p>
                     Dato / klokkeslett: {reservation?.date}
                 </p>
+                {
+                    reservation?.text && <p>Melding fra kunde: {reservation.text}</p>
+                }
 
                 <a className="flex min-w-[84px] max-w-[480px] cursor-pointer items-center justify-center overflow-hidden rounded-xl h-12 px-5 py-4 flex-1 bg-[#B2212B] text-white text-base font-bold leading-normal tracking-[0.015em]" href={`tel:${reservation?.phone}`}>
 
